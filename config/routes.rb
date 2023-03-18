29e9bb2e3e5a17d4d967d06ea6ba691723e5cc3a
ryouhei <@ryouhei_0705>
2023-03-18 16:02:41 +0900

    ログイン認証gemのdeviseの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/' => "home#top"
+  root to: "home#top"
 
   get 'input' => "home#input"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
