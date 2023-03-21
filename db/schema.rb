32379e4474745149e29e2b5b5d4ba9e37f83644c
ryouhei <@ryouhei_0705>
2023-03-21 16:30:54 +0900

    収支を表すカラム名をtotalに変更

--- db/schema.rb
+++ db/schema.rb
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2023_03_20_150113) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "record", default: 0
+    t.integer "total", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
