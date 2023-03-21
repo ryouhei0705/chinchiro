c09e4fe9082d48acd89d6d70061b83204389a6a0
ryouhei <@ryouhei_0705>
2023-03-21 17:05:59 +0900

    各リザルトを保存できるようにしたい(バグってる)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,9 +7,12 @@ class HomeController < ApplicationController
   end
   
   def add
-    user = User.find_by(id: params[:id])
-    user.result += params[:add_result].to_i
+    user = User.find_by(id: params[:userId])
+    user.total += 1
     user.save
+    user.results.create(user_id:params[:userId].to_i, performance:params[:resultPerformance].to_i, win:true)
+ 
+
     redirect_to("/")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
 
   get 'top' => "home#top"
   get 'input' => "home#input"
-  post "home/:id/add" => "home#add"
+  post "home/:userId/add" => "home#add"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
