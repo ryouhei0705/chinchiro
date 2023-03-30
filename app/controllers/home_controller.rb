e5ef8339e05b0783f3c3c9694639a4925bc135b9
ryouhei <@ryouhei_0705>
2023-03-31 00:09:40 +0900

    resultのログインユーザーのデータのみ表示

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def top
-    
+    # @user = User.all
+    @results = Result.where(user_id: current_user.id)
   end
 
   def input
@@ -10,8 +11,12 @@ class HomeController < ApplicationController
     user = User.find_by(id: params[:userId])
     user.total += 1
     user.save
-    user.results.create(user_id:params[:userId].to_i, performance:params[:resultPerformance].to_i, win:true)
- 
+
+    result = Result.new(user_id:params[:userId].to_i, performance:params[:resultPerformance].to_i, win:true)
+    result.save
+    user.results.create(performance:params[:resultPerformance].to_i, win:true)
+    user.total += 1
+    user.save
 
     redirect_to("/")
   end
--- app/models/result.rb
+++ app/models/result.rb
@@ -1,3 +1,3 @@
 class Result < ApplicationRecord
-    belongs_to :users
+    belongs_to :user
 end
