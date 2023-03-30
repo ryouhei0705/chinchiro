8b5d64185d96b05fa1125efa163a4d1ac98533bc
ryouhei <@ryouhei_0705>
2023-03-31 01:30:33 +0900

    chartkickを導入したかったが失敗している

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def top
     # @user = User.all
     @results = Result.where(user_id: current_user.id)
+    @data = [['2019-06-01', 100], ['2019-06-02', 200], ['2019-06-03', 150]]
   end
 
   def input
@@ -9,13 +10,9 @@ class HomeController < ApplicationController
   
   def add
     user = User.find_by(id: params[:userId])
-    user.total += 1
-    user.save
+    user.total += params[:resultPerformance].to_i
+    user.results.create(performance: params[:resultPerformance].to_i, win:true)
 
-    result = Result.new(user_id:params[:userId].to_i, performance:params[:resultPerformance].to_i, win:true)
-    result.save
-    user.results.create(performance:params[:resultPerformance].to_i, win:true)
-    user.total += 1
     user.save
 
     redirect_to("/")
